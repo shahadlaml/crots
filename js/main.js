b2facf3477fe81831e0a7be58998805085bf5add
shahadlaml <50049930+shahadlaml@users.noreply.github.com>
2020-01-31 16:07:17 +0300

    the new navigation is working and responsive but theses no animation yet

--- js/main.js
+++ js/main.js
@@ -4,51 +4,9 @@ $(window).load(function() {
 });
 
 //navigation
-
-function showMobileMenu(){
-var mobileVis = jQuery('.navigation').css('display');
-if(mobileVis == "none"){
-  jQuery('.navigation').slideDown('slow');
-  $('.mob_btn button').toggleClass("mob_open");
-  $('button div').css('border',' 1.5px solid #fff');
-}
-else{
-  jQuery('.navigation').slideUp('slow');
-  $('.mob_btn button').toggleClass("mob_open");
-          $('button div').css('border',' 1.5px solid #749799');
-}
-};
-jQuery('.mob_btn button').click(function(){
-  showMobileMenu();
-  return false;
+$('.menu-icon').on('click', function(){
+  $('.menu').toggleClass('hide');
 });
-
-//stiky navigation
-
-$(document).ready(function() {
-			// grab the initial top offset of the navigation
-		   	var stickyNavTop = $('.container').offset().top;
-		   	var stickyNav = function(){
-			    var scrollTop = $(window).scrollTop();
-			    if (scrollTop > stickyNavTop) {
-            $('.container').addClass('sticky');
-            $('.header').addClass('header-scroll');
-		        $('.nav-menu').addClass('nav-menu-p');
-            $("#mainlogo").attr("src","../img/logo/em-blue.svg");
-            $("#mainlogo").css("width", "40px");
-			    } else {
-            $('.container').removeClass('sticky');
-		        $('.header').removeClass('header-scroll');
-            $('.nav-menu').removeClass('nav-menu-p');
-            $("#mainlogo").attr("src","../img/logo/fullcolor.svg");
-            $("#mainlogo").css("width", "150px");
-
-			    }
-			};
-			stickyNav();
-			$(window).scroll(function() {
-				stickyNav();
-        
-			});
-
-    });
+$('.menu').on('click', function(){
+  $('.menu').toggleClass('hide');
+});
