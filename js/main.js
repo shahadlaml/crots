28fa1ab4fee9429c027b0f15d67e83fd2f9ba08f
shahadlaml <50049930+shahadlaml@users.noreply.github.com>
2020-01-31 17:17:28 +0300

    the navigation is fully done and responsive no animation tho but its fine so next up r the section cards

--- js/main.js
+++ js/main.js
@@ -3,10 +3,22 @@ $(window).load(function() {
    $('.preloader').fadeOut('slow');
 });
 
-//navigation
+//navigation btn
 $('.menu-icon').on('click', function(){
   $('.menu').toggleClass('hide');
 });
 $('.menu').on('click', function(){
   $('.menu').toggleClass('hide');
+});
+
+//nsvigation scrool effect
+$(window).on('scroll', function(){
+  if($(window).scrollTop()){
+    $('.head').addClass('scroll');
+    $("#l").attr("src","../img/logo/em-blue.svg");
+  }else{
+    $('.head').removeClass('scroll');
+    $("#l").attr("src","../img/logo/fullcolor.svg");
+
+  }
 });
