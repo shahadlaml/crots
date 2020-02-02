0ff4c10037f7c654c86a269f7667fdef32fca75e
shahadlaml <50049930+shahadlaml@users.noreply.github.com>
2020-02-02 07:50:43 +0300

    the back to tp btn is active no slide show tho i lined up the all the content next is adding the right content to the sections

--- js/main.js
+++ js/main.js
@@ -22,3 +22,21 @@ $(window).on('scroll', function(){
 
   }
 });
+
+
+//back to top button
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
